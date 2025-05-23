76c31ad25cae89269381771ac6455c36ee429367
wilyos <wilyos99@gmail.com>
2025-05-23 11:17:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let contacto = document.querySelector('#btn-contacto');
+let sobreMi = document.querySelector('#btn-sobreMi')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+contacto.onclick = () => {
+    document.getElementById("contacto").scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+
+sobreMi.onclick = () => {
+    document.getElementById("sobreMi").scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+const vcardData = `
+BEGIN:VCARD
+VERSION:3.0
+FN:Sebastian Cano
+ORG:Sistemas litográficos
+TEL;TYPE=CELL:+57 3132444781
+EMAIL:scano@sistemaslitograficos.com
+END:VCARD
+  `.trim();
+
+
+
+  const blob = new Blob([vcardData], { type: 'text/vcard' });
+  const url = URL.createObjectURL(blob);
+
+  const downloadLink = document.getElementById('downloadContact');
+  downloadLink.href = url;
