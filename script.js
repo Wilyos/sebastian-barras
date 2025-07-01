2b668c1c63dad9328be88450093e16702722f459
Wil Bohorquez <wilyos99@gmail.com>
2025-07-01 08:51:12 -0500

    Carousel

--- script.js
+++ script.js
@@ -1,7 +1,80 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let contacto = document.querySelector('#btn-contacto');
-let sobreMi = document.querySelector('#btn-sobreMi')
+let sobreMi = document.querySelector('#btn-sobreMi');
+
+
+
+// Carrusel de Clientes
+const track = document.querySelector('.carousel-track');
+const items = document.querySelectorAll('.carousel-item');
+const btnLeft = document.querySelector('.carousel-btn.left');
+const btnRight = document.querySelector('.carousel-btn.right');
+let index = 0;
+const visible = window.innerWidth < 700 ? 2 : 3;
+
+
+for (let i = 0; i < visible; i++) {
+  const clone = items[i].cloneNode(true);
+  track.appendChild(clone);
+}
+
+
+function updateCarousel(animate = true) {
+  const itemWidth = items[0].offsetWidth + 32; // 32px gap
+  if (animate) {
+    track.style.transition = 'transform 0.5s ease';
+  } else {
+    track.style.transition = 'none';
+  }
+  track.style.transform = `translateX(-${index * itemWidth}px)`;
+}
+
+/* btnLeft.addEventListener('click', () => {
+  if (index > 0) index--;
+  updateCarousel();
+});
+btnRight.addEventListener('click', () => {
+  if (index < items.length - visible) index++;
+  updateCarousel();
+});  */
+
+let autoSlide = setInterval(() => {
+  index++;
+  updateCarousel();
+  // Si llegamos al final de los clones, reseteamos sin animación
+  if (index === items.length) {
+    setTimeout(() => {
+      index = 0;
+      updateCarousel(false);
+    }, 500); // Espera a que termine la animación
+  }
+}, 2500);
+
+window.addEventListener('resize', () => {
+  visible = window.innerWidth < 700 ? 2 : 3;
+  updateCarousel();
+});
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+track.addEventListener('mouseenter', () => clearInterval(autoSlide));
+track.addEventListener('mouseleave', () => {
+  autoSlide = setInterval(() => {
+    if (index < items.length - visible) {
+      index++;
+    } else {
+      index = 0;
+    }
+    updateCarousel();
+  }, 2500);
+});
+
+updateCarousel();
+
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
